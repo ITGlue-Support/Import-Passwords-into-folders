12fae1fa304a49feb9992f9a80cce9b012d43191
IT Glue Tech Support <itgluetech@gmail.com>
2025-12-23 14:31:47 -0800

    Refactor authorization token and related functions

--- Password_import_EU_region.ps1
+++ Password_import_EU_region.ps1
@@ -77,10 +77,11 @@ function get_org_id {
         if ($($find_org.data.id).Count -gt 1) {
         
             Write-Host "Found mutliple organization with similar name. Using the organization with id:$($find_org.data.id[0])"
-        
-        }
+            return $($find_org.data.id[0])
+        }else{
 
-        return $($find_org.data.id[0])
+            return $($find_org.data.id)
+        }
     }
     catch {
         Write-Host "Make sure organization $org_name already exists in IT Glue! Error: $($_.exception.message)" -ForegroundColor Red
@@ -358,6 +359,7 @@ if ($access_token -eq $null){
     $access_token = request_data
     
 }
+
 
 
 
